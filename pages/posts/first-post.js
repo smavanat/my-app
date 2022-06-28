73a5958cc99decdebd4ccea8b18c101b3d7b5fd6
Sertorias <e.hadass@cityoflondonschool.org.uk>
2022-06-28 18:03:55 +0100

    Version 3

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,13 +2,6 @@ import Link from 'next/link';
 
 export default function FirstPost() {
   return (
-    <>
-      <h1>First Post</h1>
-      <h2>
-        <Link href={"/"} passHref>
-          <a>Back to home</a>
-        </Link>
-      </h2>
-    </>
+    console.log("PleaseWork")
   );
 }; 
