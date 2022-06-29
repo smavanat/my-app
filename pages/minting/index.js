3961997cc4d468e140fd6dc7522845c2f78647dc
Sertorias <e.hadass@cityoflondonschool.org.uk>
2022-06-29 16:46:54 +0100

    Next change 2

--- pages/minting/index.js
+++ pages/minting/index.js
@@ -4,14 +4,58 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 export default function mintingPage() {
+    const [txError, setTxError] = useState(null)
     const [likes, setLikes] = React.useState(0);
     const [walletError, setWalletError] = useState(null)
     const [currentAccount, setCurrentAccount] = useState("")
     const [correctNetwork, setCorrectNetwork] = useState(false)
 
-    function handleClick(){
-		console.log("we call this when we click")
-	}
+    async function handleClick(){
+         
+            try {
+    
+                const { ethereum } = window
+    
+                if (ethereum) {
+    
+                    const provider = new ethers.providers.Web3Provider(ethereum)
+                    const signer = provider.getSigner()
+    
+                    const nftContract = new ethers.Contract(
+                        nftContractAddress,
+                        NFT.abi,
+                        signer
+                    )
+    
+    
+                    let nftTx = await nftContract.mint(currentAccount, nftURI, ETHPriceValue)
+            console.log('Minting....', nftTx.hash)
+                    setMiningStatus(0)
+    
+                    let tx = await nftTx.wait()
+                    setLoadingState(1)
+                    // console.log('Mined!', tx)
+                    // let event = tx.events[0]
+                    // let value = event.args[2]
+                    // let tokenId = value.toNumber()
+    
+                    // console.log(
+                    // 	`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTx.hash}`
+                    // )
+    
+                    // getMintedNFT(tokenId)
+                    setMiningStatus(1)
+                    // getNFTs()
+                    // window.setTimeout(function(){location.reload()},60000)
+                    
+                } else {
+                    setWalletError('Please install MetaMask Wallet.')
+                }
+            } catch (error) {
+                // console.log('Error minting character', error)
+                setTxError(error.message)
+            }
+        }	
 
       // Checks if wallet is connected
 	const checkIfWalletIsConnected = async () => {
